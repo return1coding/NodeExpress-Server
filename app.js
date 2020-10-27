e8215b0c1a59e3b2a13c7b5ccdd885c6abfaac59
Justine Chang <return1coding@gmail.com>
2020-10-27 16:54:56 +0000

    Removed https configurations, remove link to mongoDB

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const bodyParser = require("body-parser");
 //imports for MongoDB and Humanity's TODO
 const mongoose = require('mongoose');
 const Todo = require('./toDoSchema');
-const { dbURI } = require('./dbURI');
+// const { dbURI } = require('./dbURI');
 
 
 
@@ -84,9 +84,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => console.log("Connected to MongoDB for Humanity's TODO"))
-  .catch((err) => console.log(err));
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then((result) => console.log("Connected to MongoDB for Humanity's TODO"))
+//   .catch((err) => console.log(err));
 
 
 // create a route for /projects
@@ -168,15 +168,15 @@ app.get('/get-all-todos', (req, res) => {
 
 // Listen both http & https ports
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/justinevm.ddns.net/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/justinevm.ddns.net/fullchain.pem'),
-}, app);
+// const httpsServer = https.createServer({
+//   key: fs.readFileSync('/etc/letsencrypt/live/justinevm.ddns.net/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/justinevm.ddns.net/fullchain.pem'),
+// }, app);
 
 httpServer.listen(80, () => {
   console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
-  console.log('HTTPS Server running on port 443');
-});
+// httpsServer.listen(443, () => {
+//   console.log('HTTPS Server running on port 443');
+// });
